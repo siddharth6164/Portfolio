b6e5d8c3383520bf46ffb620ab0ce1e504bcff7a
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-08 01:17:03 +0530

    Portfolio

--- src/Theme/ThemeContext.jsx
+++ src/Theme/ThemeContext.jsx
@@ -15,7 +15,6 @@ export const ThemeProvider = ({ children }) => {
   }, [theme]);
 
   const toggleTheme = () => {
-    console.log("Toggle");
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
   return(
