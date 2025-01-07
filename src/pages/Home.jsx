6971faa7fb266f80ecf9ab0835a3d8fb8546938e
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-08 00:19:47 +0530

    Portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,16 @@ import themeIcon from "../assets/sun.svg";
 import leetcode from "../assets/leetcode.svg";
 import github from "../assets/github-light.svg";
 import gfg from "../assets/gfg.svg";
+import CV from "../assets/Resume.pdf";
+import { useTheme } from "../Theme/ThemeContext";
+
 function Home() {
+  const {theme,toggleTheme}=useTheme();
   return (
     <section id="home">
       <div>
-        <img className="image1" src={image1} alt="siddharth singh" />
-        <img className="" src={themeIcon} alt="siddharth singh" />
+        <img className="image1" src={image1} alt="siddharth singh"/>
+        <img className="" src={themeIcon} alt="siddharth singh" onClick={toggleTheme}/>
       </div>
       <div className="info">
         <h1>Siddharth Singh</h1>
@@ -29,6 +33,10 @@ function Home() {
             <img src={gfg} alt="" srcset="" className="icon" />
           </a>
         </span>
+        <p>With a passion for Developing modern React web apps for commercial businesses.</p>
+        <a href={CV} download>
+          <button className="resune_btn">Resume</button>
+        </a>
       </div>
     </section>
   );
