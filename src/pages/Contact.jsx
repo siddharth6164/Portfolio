56d3b0f525df6eb3bdcb70ce0b60fc9d0e139f04
Siddharth Singh <siddhu33singh@gmail.com>
2025-01-11 15:55:09 +0530

    Portfolio

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,43 +1,37 @@
-import React from "react";
+// https://dashboard.emailjs.com/admin
+import React, {useRef}from "react";
+import emailjs from '@emailjs/browser';
 import "./contact.css";
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e)=>{
+    e.preventDefault();
+    emailjs
+      .sendForm('service_qvdjv05', 'template_eycadti', form.current, {publicKey:'4jevzycJvbb3R2BLF'})
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  }
   return (
     <section className="contact">
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form ref={form} onSubmit={sendEmail}>
         <div className="formGroup">
-          <label htmlFor="name" hidden>
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            required
-          />
+          <label htmlFor="from_name" hidden>Name</label>
+          <input type="text" name="from_name" id="from_name" placeholder="Name" required/>
         </div>
         <div>
-          <label htmlFor="email" hidden>
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email"
-            required
-          />
+          <label htmlFor="from_email" hidden>Email</label>
+          <input type="text" name="from_email" id="from_email" placeholder="Email" required/>
         </div>
         <div>
-          <label htmlFor="message" hidden>
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-            required
+          <label htmlFor="message" hidden>Message</label>
+          <textarea name="message" id="message" placeholder="Message" required
           />
         </div>
         <input type="submit" value="Submit" className="submitbtn" />
